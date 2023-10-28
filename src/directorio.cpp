bc6164167af2d4801dfcb5137e1926a70130cb46
PC\Gabriel <fgabrielortiz99@gmail.com>
2023-10-28 16:18:27 -0300

    Cambios en directorio.cpp y .h

--- src/directorio.cpp
+++ src/directorio.cpp
@@ -256,3 +256,70 @@ void destroyDirectory(TDirectorio &directorio)
         }
     }
 }
+
+
+
+//******************************Nuevas funciones *****************************************************************
+
+
+//retorna true si el directorio de nombre nombreDierctorioHijo es hijo del directorio "directorio"
+bool existChildrenDirectory(TDirectorio directorio, Cadena nombreDirectorioHijo);
+
+
+//pre-condición el directorio de nombre nombreDirectorioHijo es hijo del directorio Directorio
+//pos-condición retorna un puntero al directorio de nombre nombreDirectorioHijo
+TDirectorio moveChildrenDirectory (TDirectorio& directorio, Cadena nombreDirectorioHijo);
+
+
+//retorna un puntero de TDirectorio al padre del directorio directorio
+TDirectorio moveFatherDirectory (TDirectorio& directorio);
+
+
+//retorna un puntero de TDirectorio al directorio ROOT
+TDirectorio moveRootDirectory (TDirectorio& directorio);
+
+
+//retorna true si el directorio directorio es root
+bool isRootDirectory (TDirectorio directorio);
+
+
+//Pre-Condición del directorio de nombre nombreDirectorio no es hijo del directorio "directorio"
+//pos-condición crea un directorio vacío, de nombre nombreDirectorio, hijo del directorio "directorio"
+void createChildrenDirectory (TDirectorio& directorio, Cadena nombreDirectorio);
+
+
+//pre-condición el directorio de nombre nombreDirectorio es hijo del directorio directorio
+//pos-condición elimina el directorio de nombre nombreDirectorio que es hijo del directorio directorio
+//eliminando toda su memoria
+void removeChildrenDirectory (TDirectorio& directorio, Cadena nombreDirectorio);
+
+
+//pre-condición el directorio origen es sub-directorio del directorio "directorio"
+//pos-condición mueve el directorio origen y todo su contenido al directorio destino
+void moveSubDirectory (TDirectorio& directorio, TDirectorio origen, TDirectorio& destino);
+
+
+//pre-condición el archivo origen es sub archivo del directorio directorio
+//pos-condición se mueve el archivo TArchivo como hijo del directorio destino
+void moveSubArchive (TDirectorio& directorio, TArchivo origen, TDirectorio destino);
+
+
+//pre-condición: directorio no es el directorio ROOT
+//pos-condición: retorna un puntero al primer hermano del diretorio "directorio"
+TDirectorio firstBrotherDirectory(TDirectorio directorio);
+
+
+//pos-condición: retorna un puntero al primer hijo del directorio "directorio"
+TDirectorio firstChildrenDirectory(TDirectorio directorio);
+
+
+//Retorna true si el directorio subdir es sub-directorio del directorio "directorio" en cualquier nivel.
+bool isSubDirectoryRoot (TDirectorio directorio, Cadena ruta);
+
+
+//pos-condición imprime el directorio ejecuando DIR
+void printDirectoryDir (TDirectorio directorio);
+
+
+//pos-condición imprime el directorio ejecutando DIR /S
+void printDirectoryDirS (TDirectorio directorio);
